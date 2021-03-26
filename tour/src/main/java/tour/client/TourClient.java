a9cdb9a485c524b12439bd3f0b8aadc721ed17e8
manschko <manschko1999@googlemail.com>
2021-03-26 13:38:28 +0100

    fixes Docker localhost problem

--- tour/src/main/java/tour/client/TourClient.java
+++ tour/src/main/java/tour/client/TourClient.java
@@ -5,22 +5,20 @@ import tour.model.Weather;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Response;
-import java.util.ArrayList;
 
 public class TourClient {
 
-    private static final String WEATHER_API = "http://localhost:9080/weather/getWeather/";
-    private static final String PLACES_API = "http://localhost:9090/place/getPlaces/";
+    private static final String WEATHER_API = "http://weather:8080/weather/getWeather/";
+    private static final String PLACES_API = "http://place:8080/place/getPlaces/";
 
 
-    public static Weather[] consumeWeather(String city){
+    public static Weather[] consumeWeather(String city) {
         Client client = ClientBuilder.newClient();
         Response response = client.target(WEATHER_API + city).request().get();
         Weather[] weathers = response.readEntity(Weather[].class);
 
-        for(Weather weather : weathers)
+        for (Weather weather : weathers)
             weather.setCity(city);
 
         response.close();
@@ -33,7 +31,7 @@ public class TourClient {
         Response response = client.target(PLACES_API + city + "&" + radius).request().get();
         Sight[] sights = response.readEntity(Sight[].class);
 
-        for(Sight sight : sights){
+        for (Sight sight : sights) {
             sight.setCity(city);
             sight.setRadius(radius);
         }
